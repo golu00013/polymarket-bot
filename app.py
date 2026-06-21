12fd994bf738776f9064a68e2496080d7b0b0514
golu00013 <gholujaat644@gmail.com>
2026-06-21 11:35:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -118,3 +118,6 @@ if __name__ == "__main__":
     t = threading.Thread(target=monitor, daemon=True)
     t.start()
     app.run(host="0.0.0.0", port=8080)
+import os
+port = int(os.environ.get("PORT", 8080))
+app.run(host="0.0.0.0", port=port)
